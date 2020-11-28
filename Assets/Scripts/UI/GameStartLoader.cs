4f0d8b2cc4e357eee73d1bdd01ad2819233e806b
Soileroivas <osoro003@edu.xamk.fi>
2020-11-28 19:20:20 +0200

    Loading screen
    Loading screen lisätty, samoin splash screeniin Merrry-logo

---
+++ Assets/Scripts/UI/GameStartLoader.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameStartLoader : MonoBehaviour
+{
+
+    public int sceneToLoad;
+    public Slider progressBar;
+ 
+   
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        progressBar.GetComponent<Slider>();
+        StartCoroutine(LoadLevelAsync(sceneToLoad));
+    }
+
+
+    IEnumerator LoadLevelAsync(int sceneIndex)
+    {
+        // Delay for door close animation.
+        yield return new WaitForSeconds(1.0f);
+
+        // Loading scene async and getting loading progress.
+        AsyncOperation operation = SceneManager.LoadSceneAsync(sceneToLoad);
+
+        // While loading isn't done.
+        while (!operation.isDone)
+        {
+            
+            // Load progress to the loadbar.
+            progressBar.value = Mathf.Clamp01(operation.progress / 0.9f);
+            yield return null;
+         
+        }
+    }
+
+}
+
