324f814495072945636b270169d798476c24f5ff
RomanovMark <55138352+RomanovMark@users.noreply.github.com>
2020-11-30 11:50:09 +0200

    camera fix
    camera fix

--- Assets/Scripts/Game/CameraController.cs
+++ Assets/Scripts/Game/CameraController.cs
@@ -4,14 +4,14 @@ public class CameraController : MonoBehaviour
 {
     public Transform target;
 
-    public float smoothSpeed = 0.125f;
+    public float smoothSpeed = 3.525f;
     public Vector3 offset;
 
     // Smooth camera that follows a player.
     void LateUpdate()
     {
         // Limits camera movement for a certain range.
-        if(target.position.x < 0 && transform.position.x > -0.25f || target.position.x > 0 && transform.position.x < 0.25f || target.position.x == 0)
+        if(target.position.x < 0 && transform.position.x > -1.75f || target.position.x > 0 && transform.position.x < 1.75f || target.position.x == 0)
         {
             Vector3 desiredPosition = target.position + offset;
             Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed * Time.deltaTime);
